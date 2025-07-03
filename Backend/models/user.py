2b8aee9a927e53974cee285e6b06da529169e27c
Aakshun <aakshunpranjal06@gmail.com>
2025-07-03 15:17:29 +0530

    profile photo

--- Backend/models/user.py
+++ Backend/models/user.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Column, Integer, String
 from db.session import Base
-
+ 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    currency_preference = Column(String, default="INR") 
-
+    currency_preference = Column(String, default="INR")
+    profile_photo_url = Column(String, nullable=True)
