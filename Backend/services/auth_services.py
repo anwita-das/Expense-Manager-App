4e2e4e2ab7ba1f511c4332a6a3b34ba4fa38e11f
Anwita Das <anwitadas21@gmail.com>
2025-07-10 13:33:35 +0530

    final for loan and savings

--- Backend/services/auth_services.py
+++ Backend/services/auth_services.py
@@ -20,7 +20,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=120))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
